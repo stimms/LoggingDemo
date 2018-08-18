46dbeb65d8af97ee54ad510118aba7d41ca8ffa7
Simon Timms <stimms@gmail.com>
2018-08-18 08:01:42 -0600

    Add project files.

---
+++ LoggingDemo.Log4Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggingDemo.Log4Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
