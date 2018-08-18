7e450ef41e405369595cac290e8979ce79769b79
Simon Timms <stimms@gmail.com>
2018-08-18 13:12:39 -0600

    Adding log4net example

--- LoggingDemo.Log4Net/Program.cs
+++ LoggingDemo.Log4Net/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace LoggingDemo.Log4Net
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            log.Debug("Starting up");
+            log.Debug("Shutting down");
+            Console.ReadLine();
         }
     }
 }
---
+++ LoggingDemo.Nlog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggingDemo.Nlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
